625373fe04889384dddd66b3c1f9d05bd6f94f62
yeisonmarroquin <andresmarroquin887@gmail.com>
2023-05-04 17:48:39 -0500

    Entregable5

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,13 +1,76 @@
 import React from 'react'
-
+import './StylePoke/pokeinfo.css'
 const Pokemon = ({pokemon}) => {
+  console.log(pokemon)
   return (
     <div>
-      <img
-            className="pokemon__sprite"
+          <div className="margen">
+        <div className="rojo">
+     
+          <img className="margen_img" src="./image/Podex.png" alt="" />
+        </div>
+        <div className="padre">
+           <div className="bola2">
+            <div className="bola"> <div className="bol"></div> </div>
+           </div>
+          </div>
+        
+      </div>
+      <div className='uy'>
+      <section className='pokeinfo'>
+        <header className='pokemon_header'>
+        <img
+            className="pokemon_img"
             src={pokemon?.sprites.other["official-artwork"].front_default}
             alt=""
           />
+        </header>
+        <section className='pokemon_iden'>
+          <h1 className='pokemon_id'>#{pokemon?.id}</h1>
+          <h1 className='pokemon_name'>{pokemon?.name}</h1>
+        </section>
+        <ul className='pokemon_ul'>
+          <li className='pokemon_li'>
+            <span className='pokemon_span'>weight</span>
+            <span className='pokemon_span'>{pokemon?.weight}</span>
+          </li>
+          <li className='pokemon_li'>
+            <span className='pokemon_span'>height</span>
+            <span className='pokemon_span'>{pokemon?.height}</span>
+          </li>
+        </ul>
+        <section className='pokemon_ability'>
+          <span className='pokemon_tipo'>
+            <div>tipo</div>
+            <div className='pokemon_power'>
+               {pokemon?.types.map((objType) => (
+              
+              <span className='color'>  {objType.type.name}</span>
+             
+           ))}
+            </div>
+         
+          </span>
+          <span className='pokemon_aby'>
+            <div>abilities</div>
+            <div className='pokemon_powe'>
+               {
+              pokemon?.abilities.map(ability =>(
+                
+                   <span className='borde'>{ability.ability.name}</span>
+                
+               
+              ))
+             }
+            </div>
+         
+          </span>
+        
+    
+         
+        </section>
+      </section>
+      </div>
     </div>
   )
 }
--- src/pages/Pokeinfo.jsx
+++ src/pages/Pokeinfo.jsx
@@ -25,3 +25,4 @@ const Pokeinfo = () => {
 }
 
 export default Pokeinfo
+
